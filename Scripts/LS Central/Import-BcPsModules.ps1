9c4af7492f4a60c91813de3b7fa81c13f4aa3fc8
Arnthor Magnusson <arnthor@lsretail.com>
2021-01-04 16:38:37 +0000

    Added an example how to import BC PowerShell modules from GoC instance name.

---
+++ Scripts/LS Central/Import-BcPsModules.ps1
@@ -0,0 +1,27 @@
+<#
+    .SYNOPSIS
+        Import Business Central PowerShell modules from instance name.
+
+    .DESCRIPTION
+        This example imports the Business Central PowerShell modules for 
+        the specified instance name.
+
+    .PARAMETER InstanceName
+        Name of existing instance in Go Current.
+#>
+param(
+    $InstanceName = 'LsCentral'
+)
+$ErrorActionPreference = 'stop'
+
+# Get install details about the bc-server package for a particular instance:
+$BcServer = Get-GocInstalledPackage -Id 'bc-server' -InstanceName $InstanceName
+
+if (!$BcServer)
+{
+    throw "Specified instance ($InstanceName) does not exists or is not a Business Central instance."
+}
+
+# Import PowerShell Modules into session:
+Import-Module (Join-Path $BcServer.Info.ServerDir 'Microsoft.Dynamics.Nav.Management.dll') -Global
+Import-Module (Join-Path $BcServer.Info.ServerDir 'Microsoft.Dynamics.Nav.Apps.Management.dll') -Global
