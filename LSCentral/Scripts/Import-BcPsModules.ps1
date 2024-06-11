afbd8b482d8093d6b1f8ef5396de743afffb7374
Arnþór Magnússon <Arnthor@lsretail.com>
2024-06-11 10:07:39 +0000

    Updated import NAV cmdlets.

--- LSCentral/Scripts/Import-BcPsModules.ps1
+++ LSCentral/Scripts/Import-BcPsModules.ps1
@@ -3,7 +3,7 @@
         Import Business Central PowerShell modules from instance name.
 
     .DESCRIPTION
-        This example imports the Business Central PowerShell modules for 
+        This example imports the Business Central PowerShell modules for
         the specified instance name.
 
     .PARAMETER InstanceName
@@ -19,19 +19,7 @@ param(
 )
 $ErrorActionPreference = 'stop'
 
-# Get install details about the bc-server package for a particular instance:
-$BcServer = Get-UscInstalledPackage -Id 'bc-server' -InstanceName $InstanceName
+Import-Module LsSetupHelper\BusinessCentral\Management
 
-if (!$BcServer)
-{
-    throw "Specified instance ($InstanceName) does not exists or is not a Business Central instance."
-}
-
-$ModuleDir = $BcServer.Info.ServerDir
-if (Test-Path (Join-Path $BcServer.Info.ServerDir 'Management\Microsoft.Dynamics.Nav.Management.dll'))
-{
-    $ModuleDir = Join-Path $BcServer.Info.ServerDir 'Management'
-}
-# Import PowerShell Modules into session:
-Import-Module (Join-Path $ModuleDir 'Microsoft.Dynamics.Nav.Management.dll') -Global
-Import-Module (Join-Path $ModuleDir 'Microsoft.Dynamics.Nav.Apps.Management.dll') -Global
+Import-Module (Get-BcModulePath -InstanceName $InstanceName -Type Management) -Global
+Import-Module (Get-BcModulePath -InstanceName $InstanceName -Type Apps) -Global
